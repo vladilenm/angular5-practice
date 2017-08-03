b5989faa6424b480be27dae2719cf36c4b0e5e18
Vladilen Minin <mininvlad@gmail.com>
2017-08-03 20:36:53 +0300

    fix url

--- src/app/shared/core/base-api.service.ts
+++ src/app/shared/core/base-api.service.ts
@@ -2,7 +2,7 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 export class BaseApi {
-  private baseUrl = 'http://localhost:3000/';
+  private baseUrl = '178.208.81.216/';
 
   constructor(public http: Http) {
   }
